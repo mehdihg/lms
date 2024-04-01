a03bfce0d778fb130ac5be82d6e4c2990c692b14
mehdi <mahdihaghanee@gmail.com>
2024-04-01 21:05:48 +0330

    add icons

--- src/app/(courses)/_components/course-card.tsx
+++ src/app/(courses)/_components/course-card.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/app/_components/badge"
+import { IconArrowLeftFill, IconClock } from "@/app/_components/icons/icons"
 import { NewCouresesSummary } from "@/types/new-courses-summary-interface"
 import Image from "next/image"
 import Link from "next/link"
@@ -43,6 +44,7 @@ export const CourseCard:React.FC<CourseCardType>=({
                 <p >{subTitle}</p>
                 <div>
                 <Badge variant="warning">
+                <IconClock width={16} height={16}/>
                 {duration}
                 </Badge>
                     
@@ -51,6 +53,10 @@ export const CourseCard:React.FC<CourseCardType>=({
                     }
                 </div>
             </div>
+            <Link href={`/course/${slug}`} className="card-footer justify-center animated-icon" >
+            مشاهده جزئیات دوره
+            <IconArrowLeftFill fill="currentColor"/>
+            </Link>
         </div>
     )
 }
