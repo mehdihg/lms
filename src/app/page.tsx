a7cccb0bfc5dfb9cc6a7b9ea6416bea0836d8ea2
mehdi <mahdihaghanee@gmail.com>
2024-04-06 21:25:21 +0330

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ import { IconArrowLeftFill } from './_components/icons/icons';
 import { NewBlogSummary } from '@/types/new-blog-summary-interface';
 import { BlogPostCardList } from './(blog)/_components/blog-post-card-list';
 import { API_URL } from '@/configs/global';
+import { TestimonialList } from './_components/testmonial/testimonial-list';
+import { testimonials } from '@/data/testimonials';
 
 async function getNewCourses(count:number):Promise<NewCouresesSummary[]>{
     
@@ -110,6 +112,18 @@ export default async function Home() {
                 </div>
                 <BlogPostCardList posts={newestBlogsPosts} />
             </section>
+            <div className="relative mt-32">
+        <div className="bg-primary pointer-events-none absolute bottom-0 left-1/2 aspect-square w-1/2 -translate-x-1/2 rounded-full opacity-5 -top-52 blur-3xl"></div>
+        <h2 className="text-info relative z-0 mx-auto text-3xl font-extrabold block w-fit">
+          <span className="-z-10 w-8 h-8 absolute bg-info opacity-25 -top-2 rounded-full inline-block -right-3"></span>
+          تجربه هم‌میسرهای کلاسبن
+        </h2>
+        <p className=" mb-32 text-lg text-center mt-2">
+          تو اینجا تنها نیستی. ببین هم‌مسیرهات نظرشون در مورد دوره‌های کلاسبن
+          چیه
+        </p>
+        <TestimonialList testimonials={testimonials} />
+      </div>
     </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,10 +59,8 @@ const config: Config = {
       center:true,
       padding: {
         DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        lg: '3rem',
+        xl: '4rem',
       },
     }
   },
